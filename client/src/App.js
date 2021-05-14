38feb00187c1f2409fb20d0891983101e68af22e
Htin Linn Aung <htinlinnag1993@gmail.com>
2021-05-13 23:09:20 -0400

    Fix eslint issues

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,8 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
