46ca78573b330def5fe14315dfdfa0f36d4d497a
Valerie Huang <valeriewhuang@gmail.com>
2021-05-24 21:44:01 -0400

    create project page

---
+++ client/src/pages/CreateProject.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+const CreateProject = () => {
+  const [newName, setName] = useState('');
+  const [newServices, setServices] = useState([]);
+  const [newDescription, setDescription] = useState('');
+  // const [newServiceCount, setServiceCount] = useState(0)
+  return (
+    <>
+      <div></div>
+      <div></div>
+      <div></div>
+    </>
+  )
+}
