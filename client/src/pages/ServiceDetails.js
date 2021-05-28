812900ce9de9b976a70e60baa62f2adfb0ecb0af
jenpoole <contact.vandepool@gmail.com>
2021-05-28 16:24:54 -0400

    #24 fetch service details from DB

--- client/src/pages/ServiceDetails.js
+++ client/src/pages/ServiceDetails.js
@@ -1,5 +1,25 @@
+import {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import axios from 'axios';
+
 function ServiceDetails() {
-  return <div>Service Details</div>;
+  const {id} = useParams();
+  const [service, setService] = useState({});
+
+  useEffect(() => {
+    const findService = async () => {
+      const service = await axios.get(`http://localhost:8080/service/${id}`);
+      setService(service.data);
+    };
+    findService();
+  }, []);
+
+  return (
+    <div>
+      <h1>Service Details for {id}</h1>
+      {service.name}
+    </div>
+  );
 }
 
 export default ServiceDetails;
