11e8b45cfbeff113c518cf20e76bfa91218eebf4
jenpoole <contact.vandepool@gmail.com>
2021-05-28 17:22:10 -0400

    #24 service detail info cards

--- client/src/pages/ServiceDetails.js
+++ client/src/pages/ServiceDetails.js
@@ -1,10 +1,18 @@
 import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
+import {Card, Button} from 'antd';
+import {QuestionCircleOutlined, ReloadOutlined} from '@ant-design/icons';
+
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
+TimeAgo.addDefaultLocale(en);
 
 function ServiceDetails() {
   const {id} = useParams();
   const [service, setService] = useState({});
+
+  const timeAgo = new TimeAgo('en-US');
 
   useEffect(() => {
     const findService = async () => {
@@ -16,8 +24,37 @@ function ServiceDetails() {
 
   return (
     <div>
-      <h1>Service Details for {id}</h1>
-      {service.name}
+      <h1>
+        Service Details for {id}: {service.name}
+      </h1>
+      <Card size="small" style={{width: 300}}>
+        <p>
+          <strong>IP Address:</strong> {service.ipAddress}
+        </p>
+        <p>
+          <strong>Host:</strong> {service.host}
+        </p>
+      </Card>
+      <Card size="small" title={'Status: ' + service.status} style={{width: 300}}>
+        <p>
+          <QuestionCircleOutlined />
+        </p>
+      </Card>
+      <Card
+        size="small"
+        title={'Updated: ' + timeAgo.format(new Date() - 24 * 60 * 60 * 1000)}
+        style={{width: 300}}>
+        <p>
+          <Button
+            type="primary"
+            shape="round"
+            icon={<ReloadOutlined />}
+            size={'small'}
+            onClick={() => console.log('refreshing...')}>
+            Refresh
+          </Button>
+        </p>
+      </Card>
     </div>
   );
 }
