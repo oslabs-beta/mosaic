d756acf0912c4312ec52ca4e40e031a27e4995ab
jenpoole <contact.vandepool@gmail.com>
2021-05-28 19:25:09 -0400

    #24 update status on btn click

--- client/src/pages/ServiceDetails.js
+++ client/src/pages/ServiceDetails.js
@@ -2,7 +2,12 @@ import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
 import {Card, Button} from 'antd';
-import {QuestionCircleOutlined, ReloadOutlined} from '@ant-design/icons';
+import {
+  QuestionCircleOutlined,
+  ReloadOutlined,
+  CheckCircleOutlined,
+  WarningOutlined,
+} from '@ant-design/icons';
 
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en';
@@ -11,16 +16,28 @@ TimeAgo.addDefaultLocale(en);
 function ServiceDetails() {
   const {id} = useParams();
   const [service, setService] = useState({});
+  const [status, setStatus] = useState('');
 
   const timeAgo = new TimeAgo('en-US');
+  const [lastUpdated, setLastUpdated] = useState(
+    timeAgo.format(new Date('2021-05-28T19:45:33.903Z')),
+  );
 
   useEffect(() => {
     const findService = async () => {
       const service = await axios.get(`http://localhost:8080/service/${id}`);
       setService(service.data);
+      setStatus(service.data.status);
     };
     findService();
   }, []);
+
+  const getServiceStatus = () => {
+    const newDate = timeAgo.format(new Date());
+    const newStatus = status === 'Pending' ? 'Active' : 'Inactive';
+    setLastUpdated(newDate);
+    setStatus(newStatus);
+  };
 
   return (
     <div>
@@ -35,22 +52,19 @@ function ServiceDetails() {
           <strong>Host:</strong> {service.host}
         </p>
       </Card>
-      <Card size="small" title={'Status: ' + service.status} style={{width: 300}}>
-        <p>
-          <QuestionCircleOutlined />
-        </p>
+      <Card size="small" title={'Status: ' + status} style={{width: 300}}>
+        <p>{status === 'Pending' && <QuestionCircleOutlined />}</p>
+        <p>{status === 'Active' && <CheckCircleOutlined />}</p>
+        <p>{status === 'Inactive' && <WarningOutlined />}</p>
       </Card>
-      <Card
-        size="small"
-        title={'Updated: ' + timeAgo.format(new Date() - 24 * 60 * 60 * 1000)}
-        style={{width: 300}}>
+      <Card size="small" title={'Updated: ' + lastUpdated} style={{width: 300}}>
         <p>
           <Button
             type="primary"
             shape="round"
             icon={<ReloadOutlined />}
             size={'small'}
-            onClick={() => console.log('refreshing...')}>
+            onClick={() => getServiceStatus()}>
             Refresh
           </Button>
         </p>
